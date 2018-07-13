7e1fffb25474dd98e000ad0ba6c6f8aa2dc86375
Heorh <egor.heorh@gmail.com>
2018-07-13 16:33:37 +0300

    Add dictionary of Gender

---
+++ BezvizSystem.Pogranec.Web/Infrastructure/Log/TrackLoginFilter.cs
@@ -0,0 +1,39 @@
+﻿using BezvizSystem.BLL.DTO.Log;
+using BezvizSystem.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace BezvizSystem.Pogranec.Web.Infrastructure.Log
+{ 
+    public class TrackLoginFilter : ActionFilterAttribute
+    {
+
+        private ILogger<UserActivityDTO> _logger;
+
+        public TrackLoginFilter(ILogger<UserActivityDTO> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var identity = HttpContext.Current.User.Identity;
+
+            if (identity.IsAuthenticated)
+            {
+                _logger.Insert(new UserActivityDTO
+                {
+                    Login = identity.Name,
+                    Ip = HttpContext.Current.Request.UserHostAddress,
+                    Operation = "Вход",
+                    TimeActivity = DateTime.Now
+                });
+            }
+        }
+    }
+}
