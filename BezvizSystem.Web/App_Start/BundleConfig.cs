f0ff8004fe679769bfc96d65a0bbaedc4419e8ba
AntonVarnauski <anton.ittech@gmail.com>
2018-08-06 21:33:52 +0300

    Visitor form changes

--- BezvizSystem.Web/App_Start/BundleConfig.cs
+++ BezvizSystem.Web/App_Start/BundleConfig.cs
@@ -16,20 +16,20 @@ namespace BezvizSystem.Web
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/css/style.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
             //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/css/style.css"));
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/css/style.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/style.css"));
 
         }
     }
