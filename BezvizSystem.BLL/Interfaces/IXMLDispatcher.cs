5d1a7dac733f5f0c11981eaf3db83dd26c3fe72c
Heorh <egor.heorh@gmail.com>
2018-08-13 08:14:16 +0300

    fix #2

---
+++ BezvizSystem.BLL/Interfaces/IXMLDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using BezvizSystem.BLL.DTO;
+using BezvizSystem.BLL.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezvizSystem.BLL.Interfaces
+{
+    public interface IXMLDispatcher
+    {
+
+        Task<OperationDetails> New(VisitorDTO visitor, XMLDispatchDTO dispatch);
+        Task<OperationDetails> Update(VisitorDTO visitor, XMLDispatchDTO dispatch);
+        Task<OperationDetails> Remove(VisitorDTO visitor, XMLDispatchDTO dispatch);      
+
+    }
+}
---
+++ BezvizSystem.BLL/Services/XMLDispatcher.cs
@@ -0,0 +1,37 @@
+﻿using BezvizSystem.BLL.DTO;
+using BezvizSystem.BLL.Infrastructure;
+using BezvizSystem.BLL.Interfaces;
+using BezvizSystem.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezvizSystem.BLL.Services
+{
+    public class XMLDispatcher : IXMLDispatcher
+    {
+        IUnitOfWork _database;
+
+        public XMLDispatcher(IUnitOfWork uow)
+        {
+            _database = uow;
+        }
+
+        public Task<OperationDetails> New(VisitorDTO visitor, XMLDispatchDTO dispatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationDetails> Remove(VisitorDTO visitor, XMLDispatchDTO dispatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationDetails> Update(VisitorDTO visitor, XMLDispatchDTO dispatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
