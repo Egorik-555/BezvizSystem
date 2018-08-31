cef8f8492a74faa2a64a72921839b7d0c5615f4b
Heorh UVD <egor.heorh@gmail.com>
2018-08-31 14:22:16 +0200

    report model

--- BezvizSystem.Web/Models/Report/ReportModel.cs
+++ BezvizSystem.Web/Models/Report/ReportModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BezvizSystem.BLL.DTO.Report;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,14 +8,20 @@ namespace BezvizSystem.Web.Models.Report
 {
     public class ReportModel
     {
-        public string DateFrom { get; set; }
-        public string DateTo { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
 
-        public string AllRegistrated { get; set; }
-        public string AllArrived { get; set; }
-        public string WaitArrived { get; set; }
-        public string NotArriverd { get; set; }
-        public string AllTourist { get; set; }
-        public string AllGroup { get; set; }
+        public int? AllRegistrated { get; set; }
+        public int? AllArrived { get; set; }
+        public int? WaitArrived { get; set; }
+        public int? NotArriverd { get; set; }
+        public int? AllTourist { get; set; }
+        public int? AllGroup { get; set; }
+
+        public IEnumerable<NatAndAge> AllByNatAndAge { get; set; }
+        public IEnumerable<CountByDate> AllByDateArrivalCount { get; set; }
+        public IEnumerable<CountByCheckPoint> AllByCheckPointCount { get; set; }
+        public IEnumerable<CountByDays> AllByDaysCount { get; set; }
+        public IEnumerable<CountByOperator> AllByOperatorCount { get; set; }
     }
 }
