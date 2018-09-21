3c085d213798004d6460249399b44c2cf87b47d5
Heorh UVD <egor.heorh@gmail.com>
2018-09-21 18:08:20 +0200

    report

---
+++ BezvizSystem.Web/Scripts/google/gistogram.js
@@ -0,0 +1,17 @@
+﻿google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Dinosaur', 'Length'],
+        ['Acrocanthosaurus (top-spined lizard)', 12.2],
+        ['Albertosaurus (Alberta lizard)', 0.5]]);
+
+    var options = {
+        title: 'Lengths of dinosaurs, in meters',
+        legend: { position: 'none' },
+    };
+
+    var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
