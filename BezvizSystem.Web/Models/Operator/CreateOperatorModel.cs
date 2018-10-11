1ee18897f5a4f46269335e1d37a868b87924815c
Heorh <egor.heorh@gmail.com>
2018-10-11 23:56:04 +0300

    view create operator

--- BezvizSystem.Web/Models/Operator/CreateOperatorModel.cs
+++ BezvizSystem.Web/Models/Operator/CreateOperatorModel.cs
@@ -13,19 +13,19 @@ namespace BezvizSystem.Web.Models.Operator
         public string Id { get; set; }
         [HiddenInput(DisplayValue = false)]
         public string UserName { get; set; }
-        [Required(ErrorMessage = "Наименование не введено")]
+        [Required(ErrorMessage = "Укажите наименование")]
         [Display(Name = "Наименование")]
         public string Transcript { get; set; }
 
-        [Required(ErrorMessage = "УНП не введён")]
-        [StringLength(9, MinimumLength = 9, ErrorMessage = "Длина строки должна быть 9 символов")]
-        [RegularExpression(@"[0-9]*", ErrorMessage = "Допустимы только цифровые символы")]
+        [Required(ErrorMessage = " Укажите УНП")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "Длина УНП должна быть 9 символов")]
+        [RegularExpression(@"[0-9]*", ErrorMessage = "В УНП допустимы только цифровые символы")]
         [Display(Name = "УНП")]
         public string UNP { get; set; }
 
-        [Required(ErrorMessage = "ОКПО не введён")]
-        [StringLength(10, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 10 символов")]
-        [RegularExpression(@"[0-9]*", ErrorMessage = "Допустимы только цифровые символы")]
+        [Required(ErrorMessage = "Укажите ОКПО")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Длина ОКПО должна быть от 3 до 10 символов")]
+        [RegularExpression(@"[0-9]*", ErrorMessage = "В ОКПО допустимы только цифровые символы")]
         [Display(Name = "ОКПО")]
         public string OKPO { get; set; }
 
