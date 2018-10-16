f5152509c3f66c91c9536e1f28562adbd0e3d4bd
Heorh <egor.heorh@gmail.com>
2018-10-16 22:49:15 +0300

    filter in anketa

---
+++ BezvizSystem.Web/Scripts/MyScripts/ShowPanels.js
@@ -0,0 +1,13 @@
+﻿
+function showPanel(button, panel) {
+
+    var button = document.getElementById(button);
+    var panel = document.getElementById(panel);
+
+    button.onclick = function () {
+        if (panel.style.display != 'none')
+            panel.style.display = 'none';
+        else panel.style.display = '';
+    }
+
+}
