fce0b70733e594ad18354d6600230acbd004d132
Heorh <egor.heorh@gmail.com>
2018-11-09 08:16:56 +0300

    home

--- BezvizSystem.Pogranec.Web/Models/Report/ViewTable1InExcel.cs
+++ BezvizSystem.Pogranec.Web/Models/Report/ViewTable1InExcel.cs
@@ -8,20 +8,19 @@ namespace BezvizSystem.Pogranec.Web.Models.Report
 {
     public class ViewTable1InExcel
     {
-        public class ViewAnketaExcel
-        {
-            [Display(Name = "Страна")]
-            public string Natiolaty { get; set; }
-            [Display(Name = "Мужчин младше 18")]
-            public string ManLess18 { get; set; }
-            [Display(Name = "Мужчин старше 18")]
-            public string ManMore18 { get; set; }
-            [Display(Name = "Женщин младше 18")]
-            public string WomanLess18 { get; set; }
-            [Display(Name = "Женщин старше 18")]
-            public string WomanMore18 { get; set; }
-            [Display(Name = "Итого")]
-            public string All { get; set; }          
-        }
+
+        [Display(Name = "Страна")]
+        public string Natiolaty { get; set; }
+        [Display(Name = "Мужчин младше 18")]
+        public string ManLess18 { get; set; }
+        [Display(Name = "Мужчин старше 18")]
+        public string ManMore18 { get; set; }
+        [Display(Name = "Женщин младше 18")]
+        public string WomanLess18 { get; set; }
+        [Display(Name = "Женщин старше 18")]
+        public string WomanMore18 { get; set; }
+        [Display(Name = "Итого")]
+        public string All { get; set; }
+
     }
 }
