e261ff63bc80131ffc103e4a27eb44a1f0473ed0
Heorh <egor.heorh@gmail.com>
2018-11-14 22:43:38 +0300

    logger

---
+++ BezvizSystem.BLL/Services/Log/MapperLogLProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using BezvizSystem.BLL.DTO.Log;
+using BezvizSystem.BLL.DTO.XML;
+using BezvizSystem.DAL.Entities;
+using BezvizSystem.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezvizSystem.BLL.Services.Log
+{
+    class MapperLogLProfile : Profile
+    {
+         
+        public MapperLogLProfile()
+        {
+            CreateMap<BezvizSystem.DAL.Entities.Log, LogDTO>();
+            CreateMap<LogDTO, BezvizSystem.DAL.Entities.Log>();
+        }
+    }
+}
