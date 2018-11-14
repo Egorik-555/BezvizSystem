7c1efc5720dde798035fa06d67c7a348842e0fb6
Heorh <egor.heorh@gmail.com>
2018-11-14 11:48:14 +0300

    logger

---
+++ BezvizSystem.BLL/Services/Log/Logger.cs
@@ -0,0 +1,31 @@
+﻿using BezvizSystem.BLL.DTO.Log;
+using BezvizSystem.BLL.Interfaces.Log;
+using BezvizSystem.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezvizSystem.BLL.Services.Log
+{
+    public class Logger : ILogger
+    {
+        private IUnitOfWork _base;
+
+        public Logger(IUnitOfWork uow)
+        {
+            _base = uow;
+        }
+
+        public LogDTO Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLog(LogDTO log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
