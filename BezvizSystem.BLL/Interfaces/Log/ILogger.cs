896a9882ca698c516074c6f73b786c673c7a1201
Heorh <egor.heorh@gmail.com>
2018-11-14 11:28:31 +0300

    log

---
+++ BezvizSystem.BLL/Interfaces/Log/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using BezvizSystem.BLL.DTO.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezvizSystem.BLL.Interfaces.Log
+{
+    interface ILogger
+    {
+        void WriteLog(LogDTO log);
+        LogDTO Read();
+    }
+}
