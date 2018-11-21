a86293d35ad4773c6fb4ac65970e907a34dafcad
Heorh <egor.heorh@gmail.com>
2018-11-21 23:43:51 +0300

    logs

---
+++ BezvizSystem.Pogranec.Web/Models/Log/LogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BezvizSystem.Pogranec.Web.Models.Log
+{
+    public class LogModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+       // public string Type { get; set; }
+
+        public string Ip { get; set; }
+        public DateTime? DateActivity { get; set; }
+        public string TextActivity { get; set; }
+    }
+}
